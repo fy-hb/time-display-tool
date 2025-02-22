a8a02270fd87e1a199ac997e2ff989b9793ae947
F. ICE <fyhb233@gmail.com>
2025-02-22 18:31:17 +0800

    [25-02-22] add flake.nix

---
+++ flake.nix
@@ -0,0 +1,26 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = {
+    nixpkgs,
+    flake-utils,
+    ...
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      devShells.default = pkgs.mkShell {
+        packages = with pkgs; [
+            python312
+            python312Packages.ntplib
+            python312Packages.tkinter
+        ];
+        shellHook = ''
+          exec fish
+        '';
+      };
+    });
+}
